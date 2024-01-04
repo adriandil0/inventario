c07b517ede4d5c817c7654f815a22959e3dbdc3d
adriandil0 <152308151+adriandil0@users.noreply.github.com>
2024-01-04 08:00:40 -0500

    archivos a

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-import funciones.clientes as c
-# import funciones.corefile as core 
-if _name_ == '_mian_':
-    if(c.cf.checkFile(c.cf.MY_DATABASE)):
-        print('ok')
-    else:
-        print("Error")
+import os 
+opciones = ['Gestor Clientes', 'Gestor Proveedores', 'Gestor Productos', 'Salir']
+opcionClientes = ['Nuevo Cliente', 'Borrar Cliente', 'Editar Cliente', 'Buscar', 'Menu Principal']
+def generarMainMenu():
+    header = """ 
+    +++++++++++++++++++++++++++++++++
+    + SISTEMA GESTOR DE INVENTARIOS +
+    +++++++++++++++++++++++++++++++++
+    """
+    print(header)
+    for i, item in enumerate(opciones):
+        print(f'{(i+1)}-{item}')
+def generarClienteMenu():
+    isActiveCustomer = True
+    header="""
+    ++++++++++++++++++++++++++++++
+    + ADMINISTRACION DE CLIENTES +
+    ++++++++++++++++++++++++++++++
+    """
+    while (isActiveCustomer):
+        os.system('cls')
+        print(header)
+        for i, item in enumerate(opcionClientes):
+            print(f'{(i+1)}- {item}')
+        try:
+            op = int (input(":)_"))
+        except ValueError:
+            print("Error en el tipo de dato")
+        else:
+            if(op == 1):
+                pass
+            if(op == 2):
+                pass
+            if(op == 3):
+                pass
+            if(op == 4):
+                pass
+            if(op == 5):
+                isActiveCustomer = False
+
